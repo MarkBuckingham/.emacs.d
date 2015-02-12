ec8f7765cd235e7fa7cb9dc83e2da676eb0275b7
mark buckingham <mark.buckingham@courtview.com>
2015-02-12 14:42:50 -0500

    added color theme init

--- customizations/ui.el
+++ customizations/ui.el
@@ -25,6 +25,7 @@
 ;; for a great explanation of emacs color themes.
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Custom-Themes.html
 ;; for a more technical explanation.
+(color-theme-initialize)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (add-to-list 'load-path "~/.emacs.d/themes")
 (load-theme 'tomorrow-night-bright t)
